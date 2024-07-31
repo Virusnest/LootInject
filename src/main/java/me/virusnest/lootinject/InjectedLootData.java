2c15d3263b6f901135e200495709e834e9a4c184
Virusnest <kai.maloney.au@gmail.com>
2024-07-31 19:02:22 +1000

    Update

---
+++ src/main/java/me/virusnest/lootinject/InjectedLootData.java
@@ -0,0 +1,42 @@
+package me.virusnest.lootinject;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.mojang.serialization.Dynamic;
+import com.mojang.serialization.JsonOps;
+import net.fabricmc.fabric.api.loot.v2.FabricLootTableBuilder;
+import net.fabricmc.fabric.impl.resource.loader.FabricLifecycledResourceManager;
+import net.minecraft.loot.LootTable;
+import net.minecraft.resource.JsonDataLoader;
+import net.minecraft.resource.Resource;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.util.Identifier;
+
+import java.io.BufferedReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class InjectedLootData {
+    public Map<Identifier, LootTable> injectedTables = new HashMap<>();
+    public static String INJECT_PATH = "injected_loot_table";
+    public void LoadInjectedTables(ResourceManager manager) {
+        manager.findResources(INJECT_PATH, path -> path.getPath().endsWith(".json")).forEach(this::LoadInjectedTable);
+    }
+    public void LoadInjectedTable(Identifier id,Resource resource){
+
+        //remove json and injected_loot_table from the path only from the end and start
+        id = Identifier.of(id.getNamespace(), id.getPath().substring(INJECT_PATH.length() + 1, id.getPath().length() - 5));
+        Lootinject.LOGGER.info(id.toString());
+        try {
+            //load the json file to a LootTable Object
+            BufferedReader reader = new BufferedReader(resource.getReader());
+            JsonObject json = new JsonParser().parse(reader).getAsJsonObject();
+            LootTable table = LootTable.CODEC.parse(new Dynamic<>(JsonOps.INSTANCE, json)).result().get();
+            injectedTables.put(id, table);
+            // Do something with the table
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/me/virusnest/lootinject/Lootinject.java
@@ -0,0 +1,53 @@
+package me.virusnest.lootinject;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Items;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.condition.AnyOfLootCondition;
+import net.minecraft.loot.entry.DynamicEntry;
+import net.minecraft.loot.entry.GroupEntry;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LootTableEntry;
+import net.minecraft.resource.Resource;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.resource.ResourceType;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.spongepowered.asm.mixin.Interface;
+
+import java.io.InputStream;
+
+public class Lootinject implements ModInitializer {
+
+    public static final String MOD_ID = "lootinject";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static InjectedLootData data = new InjectedLootData();
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Initalised");
+        ServerLifecycleEvents.START_DATA_PACK_RELOAD.register((server, serverResourceManager) -> {
+            data.LoadInjectedTables(serverResourceManager);
+        });
+        ServerLifecycleEvents.SERVER_STARTING.register((server) -> {
+            data.LoadInjectedTables(server.getResourceManager());
+        });
+        LootTableEvents.MODIFY.register((table, builder, source, wrapper) -> {
+            // Let's only modify built-in loot tables and leave data pack loot tables untouched by checking the source.
+            // We also check that the loot table ID is equal to the ID we want.
+            LOGGER.info(table.getValue().toString());
+            if (data.injectedTables.containsKey(table.getValue()) &&source.isBuiltin()) {
+                // We make the pool inject table from data
+                LootPool poolBuilder = LootPool.builder().with(LootTableEntry.builder(data.injectedTables.get(table.getValue()))).build();
+
+                builder.pool(poolBuilder);
+            }
+        });
+    }
+
+}
